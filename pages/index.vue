296991135053a4273e47ed289db5c611cb619e72
daniel verdu <iamadeveloper.es@gmail.com>
2025-03-02 21:55:16 +0100

    Add initial setup

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+
+const onSubmit = () => {
+  
+}
+</script>
+
+<template>
+  <div>
+    <form @submit.prevent="onSubmit">
+      <label for="userName">User Name</label>
+      <input type="text" name="userName" id="userName">
+      <label for="pass">Password</label>
+      <input type="password" name="pass" id="pass">
+      <button type="submit">Send</button>
+    </form>
+  </div>
+</template>
+
+<style>
+
+</style>
