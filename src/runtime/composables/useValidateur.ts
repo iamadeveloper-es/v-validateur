9640b939b5d1fddb0e8e43fe3e945d695398c4d3
Daniel Verdu <iamadeveloper.es@gmail.com>
2025-03-09 23:05:26 +0100

    Refeactor validateur composable functionallity

--- src/runtime/composables/useValidateur.ts
+++ src/runtime/composables/useValidateur.ts
@@ -1,3 +1,4 @@
+import type { Rule, ValidationResult } from "./validateurTypes";
 
 export const useValidateur = () => {
   
@@ -8,37 +9,68 @@ export const useValidateur = () => {
   const formErrorsSchema = reactive(new Map())
   const validationMode = ref('')
 
-  const isFormValid = computed(() => formErrorsSchema.size)
+  const isFormValid = computed(() => formErrorsSchema.size === 0)
   const validationErrors = computed(() => Object.fromEntries(formErrorsSchema))
 
-  const initValidator = (el: HTMLFormElement , schema: FormSchema, mode?: ValidationMode) => {
+  const createValidateur = (el: HTMLFormElement , schema: FormSchema, mode?: ValidationMode) => {
     formToValidate.value = el
     formSchema = schema
+
     validationMode.value = mode || 'onSubmit'
+
+    if(validationMode.value === 'instant'){
+      instantValidation()
+    }
+  }
+
+  const instantValidation = () => {
+    for (const key in formSchema) {
+      watch(
+        () => formSchema[key].value, // Observamos solo el 'value' de cada campo
+        () => {
+          validateField(key); // Llamamos a la validación solo para el campo específico
+        }
+      );
+    }
   }
 
   const validate = () => {
     for(const key in formSchema){
-      const validationsStatus = validateField(key)
-      const isValid = validationsStatus.every((validation: object) => validation.isValid)
+      validateField(key)
+    }
 
-      if(!isValid){
-        const firstError = validationsStatus.find(item => item.isValid === false)
-        formErrorsSchema.set(key, firstError?.message)
-      }
-      else{
-        formErrorsSchema.delete(key)
-      }
+    if(validationMode.value === 'instantOnSubmit'){
+      instantValidation()
+    }
+
+    if(!isFormValid.value){
+      const firstErrorKey = formErrorsSchema.keys().next().value;
+      scrollToField(firstErrorKey)
     }
   }
 
   const validateField = (key: string) => {
-    const field: Rule = formSchema[key]
+    const field = formSchema[key]
     const value = field.value
-    return field.validations.map((rule: string) => {
+    let validationsStatus: ValidationResult[] = []
+    field.validations.forEach((rule: string) => {
       const formatedRules = parseRule(rule)
-      return validationRules[formatedRules.rule](value, formatedRules.args)
+      validationsStatus.push(validationRules[formatedRules.rule](value, formatedRules.args))
     })
+
+    checkValidity(key, validationsStatus)
+  }
+
+  const checkValidity = (key: string, validations: ValidationResult[]) => {
+    const isValid = validations.every((validation: ValidationResult) => validation.isValid)
+
+    if(!isValid){
+      const firstError = validations.find(item => item.isValid === false)
+      formErrorsSchema.set(key, firstError?.message)
+    }
+    else{
+      formErrorsSchema.delete(key)
+    }
   }
 
   const parseRule = (rule: string) => {
@@ -49,9 +81,20 @@ export const useValidateur = () => {
     };
   }
 
+  const scrollToField = (fieldName: string) => {
+    const inputField = formToValidate.value?.value?.querySelector(`input[name="${fieldName}"]`) as HTMLInputElement
+    nextTick(() => {
+      inputField.scrollIntoView({ behavior: 'smooth', block: 'center' });
+      // inputField.focus();
+      setTimeout(() => {
+        inputField.focus();
+      }, 300);
+    });
+  };
+
 
   return {
-    initValidator,
+    createValidateur,
     validate,
     isFormValid,
     validationErrors
