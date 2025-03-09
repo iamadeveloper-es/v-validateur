d26d36e7f26a230ca1025a7930afa3ac559ff201
daniel verdu <iamadeveloper.es@gmail.com>
2025-03-09 13:01:28 +0100

    Adde initial validation types

---
+++ composables/validateurTypes.ts
@@ -0,0 +1,27 @@
+export type ValidationMode = 'onSubmit' | 'instant' | 'instantOnSubmit'
+
+export type ValidatorConfig = {
+    el: HTMLFormElement | null,
+    schema: object,
+    mode?: ValidationMode
+}
+
+export type Rule = {
+    value: unknown,
+    validations: string[]
+}
+
+export type FormSchema = {
+    [key: string]: Rule
+}
+
+type ValidationResult = {
+    isValid: boolean;
+    message: string;
+};
+
+type ValidationFunction = (value: unknown, args?: (string[] | number[])) => ValidationResult;
+
+export type ValidationRule = {
+    [key: string]: ValidationFunction;
+};
